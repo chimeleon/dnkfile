d812ffe7f0572f303c8d8db09623eac86cf81636
gimseunghyeon <chimeleon99@gmail.com>
2021-11-09 23:16:14 +0900

    init commit

---
+++ src/main/java/com/dnkdream/dnkfile/DnkfileApplication.java
@@ -0,0 +1,13 @@
+package com.dnkdream.dnkfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnkfileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DnkfileApplication.class, args);
+    }
+
+}
